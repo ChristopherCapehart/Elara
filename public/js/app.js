ec1e9e039f528e69d4b75c446d0b0291d58525f1
ChristopherCapehart <christophercapehart0011@gmail.com>
2024-01-05 15:45:28 +0000

    Create app and login components

---
+++ public/js/app.js
@@ -0,0 +1,4 @@
+// load login
+$("#app").on("loaded", () => {
+  $("#login").load("/public/components/login.html");
+});
---
+++ public/js/main.js
@@ -0,0 +1,2 @@
+// load app
+$("#app").load("/public/components/app.html", () => $("#app").trigger("loaded"));
