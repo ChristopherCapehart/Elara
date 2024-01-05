a0e0ecfb31a011b4cb62d4b7da5f784ab27d2342
ChristopherCapehart <christophercapehart0011@gmail.com>
2024-01-05 14:46:56 +0000

    Create server

---
+++ index.js
@@ -0,0 +1,26 @@
+// dependencies
+const express = require("express");
+const path = require("path");
+
+// create app
+const app = express();
+
+// app setup
+app.set("view-engine", "ejs");
+
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routers
+const mainRouter = require("./routes/main");
+const apiRouter = require("./routes/api");
+
+app.use("/", mainRouter);
+app.use("/api", apiRouter);
+
+// database setup
+const port = process.env.PORT || 3001;
+
+// init app
+app.listen(port, () => console.log(`Server listening on port ${port}.`));
